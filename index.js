56e1d343203201e4e8cc64e6fb250d94c53a107b
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-03-12 07:50:43 +0300

    Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+function findMatching(array, string){
+    let list = [];
+    for(const driver of array){
+        if(driver.toLowerCase() === string.toLowerCase()){
+            list.push(driver);
+        }
+    }
+    return list;
+}
+
+function fuzzyMatch(array, string){
+    let driverList = [];
+    for(const driver of array){
+        if(driver.charAt(0) === string.charAt(0)){
+            driverList.push(driver);
+        }
+    }
+    return driverList;
+}
+
+function matchName(array, string){
+    const filteredList = array.filter(obj => obj.name.toLowerCase() === string.toLowerCase());
+    return filteredList;
+}
